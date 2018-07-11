62c3403d924ed69d0b8b40419ac8328697ea5bbc
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-11 11:23:21 +0200

    Keep on Working

--- app/index.py
+++ app/index.py
@@ -1,9 +1,12 @@
-import app.tests.network_speedtest, app.api.cloudmon
+import app.tests.network_speedtest, app.api.cloudmon, app.tests.file_speedtest, os
 
 print('Welcome!')
 print('Now i will perform the tests.')
 print('First of all the network speed app')
 result = app.tests.network_speedtest.perform_test()
 print(result)
-app.api.cloudmon.send_results_to_cloud_mon("1234abc", 'network_upload', result.upload)
-app.api.cloudmon.send_results_to_cloud_mon("1234abc", 'network_download', result.download)
+#app.api.cloudmon.send_results_to_cloud_mon("1234abc", 'network_upload', result.upload)
+#app.api.cloudmon.send_results_to_cloud_mon("1234abc", 'network_download', result.download)
+
+speed = app.tests.file_speedtest.diskspeedmeasure(os.getcwd())
+print("Disk writing speed: %.2f Mbytes per second" % speed)
