3c59044e7055148e072968b5bfea0d3a5525b793
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-24 15:26:46 +0200

    Fix

--- api/cloudmon.py
+++ api/cloudmon.py
@@ -3,17 +3,18 @@ import os
 import requests
 
 
-def send_results_to_cloud_mon(check: str, result: float):
+def send_results_to_cloud_mon(check: str, result):
     _config = config.Config()
     url = _config.get_api_url(os.getenv('CLOUD_MON_ENVIRONMENT', 'stageing'))
     api_key = os.getenv('CLOUD_MON_API_KEY')
     provider = os.getenv('CLOUD_MON_PROVIDER')
     print(api_key)
-    r = requests.post(url, data={
-        check: check,
-        result: result,
-        api_key: api_key,
-        provider: provider
-    })
+    payload = {
+        'check': check,
+        'result': result,
+        'api_key': api_key,
+        'provider': provider
+    }
+    r = requests.post(url, data=payload)
     print(r)
     print(r.text)
