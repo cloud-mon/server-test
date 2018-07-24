2a17c3848f7cc6f321e994e927f6f9c15d174453
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-24 14:54:14 +0200

    Prepare for Release

--- api/cloudmon.py
+++ api/cloudmon.py
@@ -3,13 +3,13 @@ import os
 import requests
 
 
-def send_results_to_cloud_mon(check_id: str, test: str, result: float):
+def send_results_to_cloud_mon(check_id: str, check: str, result: float):
     _config = config.Config()
     url = (_config.get_api_url(os.getenv('CLOUD_MON_ENVIRONMENT', 'stageing'), check_id))
     api_key = os.getenv('CLOUD_MON_API_KEY')
     provider = os.getenv('CLOUD_MON_PROVIDER')
     requests.post(url, {
-        check: test,
+        check: check,
         result: result,
         api_key: api_key,
         provider: provider
