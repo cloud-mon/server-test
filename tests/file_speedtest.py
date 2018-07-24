f0a8815efda78e650515cfb6ef101aaa8220f045
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-24 10:18:08 +0200

    Update Readme

--- tests/file_speedtest.py
+++ tests/file_speedtest.py
@@ -4,12 +4,12 @@ from subprocess import call
 
 
 def install_fio():
-    call('apt install fio -y')
+    call(['apt', 'install', 'fio', '-y'])
 
 
 def perform_test():
     tmp = ''
     call(
         'fio --output-format=json --filesize=3g --ioengine=libaio --filename=fio_test_file --overwrite=1 --invalidate=0 --direct=1 --randrepeat=0 --iodepth=64 --size=4097152k --blocksize=4k --name=random_write --rw=randwrite --end_fsync=1 --name=random_read --stonewall --rw=randread --name=mixed_randrw --stonewall --rw=randrw --rwmixread=90 --rwmixwrite=10 --end_fsync=1',
-        stdout=tmp)
+        stdout=tmp, shell=True)
     return tmp
