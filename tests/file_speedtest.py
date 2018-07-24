51ef86189c3290c1fadc8dcdfbaccf402fdefb2a
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-24 10:25:42 +0200

    Update Readme

--- tests/file_speedtest.py
+++ tests/file_speedtest.py
@@ -11,8 +11,7 @@ def perform_test():
     fileName = "tmp_result.json"
     f = open(fileName, "w+")
     print(call(
-        'fio --output-format=json --filesize=3g --ioengine=libaio --filename=fio_test_file --overwrite=1 --invalidate=0 --direct=1 --randrepeat=0 --iodepth=64 --size=4097152k --blocksize=4k --name=random_write --rw=randwrite --end_fsync=1 --name=random_read --stonewall --rw=randread --name=mixed_randrw --stonewall --rw=randrw --rwmixread=90 --rwmixwrite=10 --end_fsync=1',
-        stdout=f, shell=True))
+        'fio --output-format=json --filesize=3g --ioengine=libaio --filename=fio_test_file --overwrite=1 --invalidate=0 --direct=1 --randrepeat=0 --iodepth=64 --size=4097152k --blocksize=4k --name=random_write --rw=randwrite --end_fsync=1 --name=random_read --stonewall --rw=randread --name=mixed_randrw --stonewall --rw=randrw --rwmixread=90 --rwmixwrite=10 --end_fsync=1', shell=True))
     tmp = f.read()
     print(tmp)
     os.remove(fileName)
