176255f0e92e84da1d72443a60595d1ef874d9e9
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-24 10:29:11 +0200

    Update Readme

--- tests/file_speedtest.py
+++ tests/file_speedtest.py
@@ -13,9 +13,8 @@ def perform_test():
     print(call(
         'fio --output-format=json --filesize=3g --ioengine=libaio --filename=fio_test_file --overwrite=1 --invalidate=0 --direct=1 --randrepeat=0 --iodepth=64 --size=4097152k --blocksize=4k --name=random_write --rw=randwrite --end_fsync=1 --name=random_read --stonewall --rw=randread --name=mixed_randrw --stonewall --rw=randrw --rwmixread=90 --rwmixwrite=10 --end_fsync=1 >> ' + fileName,
         shell=True))
-    f = open(fileName, "w+")
+    f = open(fileName, "r+")
     tmp = f.read()
     print(tmp)
-    os.remove(fileName)
 
     return tmp
