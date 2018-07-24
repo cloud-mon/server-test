521bcabd79118212fdf8991482e0f0eb93ca47d6
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-24 15:38:09 +0200

    Fix

--- tests/file_speedtest.py
+++ tests/file_speedtest.py
@@ -15,8 +15,7 @@ def perform_test():
         'fio --output-format=json --filesize=3g --ioengine=libaio --filename=fio_test_file --overwrite=1 --invalidate=0 --direct=1 --randrepeat=0 --iodepth=64 --size=4097152k --blocksize=4k --name=random_write --rw=randwrite --end_fsync=1 --name=random_read --stonewall --rw=randread --name=mixed_randrw --stonewall --rw=randrw --rwmixread=90 --rwmixwrite=10 --end_fsync=1 >> ' + fileName,
         shell=True))
     f = open(fileName, "r+")
-    tmp = f.read()
-    data = json.load(tmp)
+    data = json.load(f)
     result = {
         'random_write': data['jobs'][0]['write']['iops'],
         'random_read': data['jobs'][1]['read']['iops'],
