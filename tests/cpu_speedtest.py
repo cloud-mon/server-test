602f272bc6ea3bf3e5ecf70c4080c712900b6b7f
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-25 09:58:38 +0200

    Add Coremark Test

--- tests/cpu_speedtest.py
+++ tests/cpu_speedtest.py
@@ -15,8 +15,10 @@ def perform_test():
     except OSError:
         pass
     print(call(
-        'cd coremark && make PORT_DIR=linux64 ITERATIONS=200000 XCFLAGS="-g -O2 -DMULTITHREAD=2 -DUSE_PTHREAD -DPERFORMANCE_RUN=1" REBUILD=1',
+        'cd coremark && make PORT_DIR=linux64 ITERATIONS=200000 XCFLAGS="-g -O2 -DMULTITHREAD=' + os.getenv(
+            'SERVER_CORES', 2) + ' -DUSE_PTHREAD -DPERFORMANCE_RUN=1" REBUILD=1',
         shell=True))
     f = open(fileName, "r+")
-    print(f.read())
-
+    data = f.read()
+    data = data.replace('2K performance run parameters for coremark.', '')
+    print(data)
