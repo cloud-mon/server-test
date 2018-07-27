c695b3a2d1160ce0034db6b5c90a2d31918f344a
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-27 07:03:22 +0200

    Test PHP Build Time

--- test.py
+++ test.py
@@ -3,13 +3,16 @@ import api.cloudmon
 import os
 import sys
 import tests.php_build_speedtest
+import tests.cpu_speedtest
 
 
 def main():
     print('Welcome at the test !')
     print(sys.argv)
-   # tests.php_build_speedtest.clone_php_src()
-    tests.php_build_speedtest.perform_test()
+    # tests.php_build_speedtest.clone_php_src()
+    # tests.php_build_speedtest.perform_test()
+    print(tests.cpu_speedtest.install_coremark())
+    print(tests.cpu_speedtest.perform_test())
 
 
 if __name__ == "__main__":
