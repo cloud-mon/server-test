12b16d195f74b54849766c814063c0cb88124323
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-31 09:52:16 +0200

    Add possibility to test volumes

--- tests/file_speedtest.py
+++ tests/file_speedtest.py
@@ -22,7 +22,7 @@ def perform_test(volume=False):
         file = '/mnt/test-volume/' + file
         path = '/mnt/test-volume/'
         file_name = path + "tmp_result.json"
-        filesize = "1"
+        filesize = "3"
 
     print(call(
         'fio --output-format=json --filesize=' + filesize + 'g --ioengine=libaio --filename=' + file + ' --overwrite=1 --invalidate=0 --direct=1 --randrepeat=0 --iodepth=64 --size=4097152k --blocksize=4k --name=random_write --rw=randwrite --end_fsync=1 --name=random_read --stonewall --rw=randread --name=mixed_randrw --stonewall --rw=randrw --rwmixread=90 --rwmixwrite=10 --end_fsync=1 >> ' + file_name,
