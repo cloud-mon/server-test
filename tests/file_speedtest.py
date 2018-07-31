8c19aa61681300d41cbac6483f1ee94e83c26d44
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-31 09:52:04 +0200

    Add possibility to test volumes

--- tests/file_speedtest.py
+++ tests/file_speedtest.py
@@ -21,12 +21,13 @@ def perform_test(volume=False):
     if volume:
         file = '/mnt/test-volume/' + file
         path = '/mnt/test-volume/'
+        file_name = path + "tmp_result.json"
         filesize = "1"
 
     print(call(
-        'fio --output-format=json --filesize='+filesize+'g --ioengine=libaio --filename=' + file + ' --overwrite=1 --invalidate=0 --direct=1 --randrepeat=0 --iodepth=64 --size=4097152k --blocksize=4k --name=random_write --rw=randwrite --end_fsync=1 --name=random_read --stonewall --rw=randread --name=mixed_randrw --stonewall --rw=randrw --rwmixread=90 --rwmixwrite=10 --end_fsync=1 >> ' + file_name,
+        'fio --output-format=json --filesize=' + filesize + 'g --ioengine=libaio --filename=' + file + ' --overwrite=1 --invalidate=0 --direct=1 --randrepeat=0 --iodepth=64 --size=4097152k --blocksize=4k --name=random_write --rw=randwrite --end_fsync=1 --name=random_read --stonewall --rw=randread --name=mixed_randrw --stonewall --rw=randrw --rwmixread=90 --rwmixwrite=10 --end_fsync=1 >> ' + file_name,
         shell=True))
-    f = open(path + file_name, "r+")
+    f = open(file_name, "r+")
     data = json.load(f)
     result = {
         'random_write': data['jobs'][0]['write']['iops'],
