d6369cfca2f24a9b99bb869f8f47f48ef4855e56
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-08-13 10:50:07 +0200

    add php build test

--- tests/php_build_speedtest.py
+++ tests/php_build_speedtest.py
@@ -10,37 +10,16 @@ def clone_php_src():
 
 def perform_test():
     file_name = "php-src/test_result.log"
+    sec_start = time.time()
     try:
         os.remove(file_name)
     except OSError:
         pass
-    print(call(
-        'cd php-src && ./buildconf -f >> /dev/null && ./configure >> /dev/null ',
-        shell=True))
+    call('cd php-src && ./buildconf -f >> /dev/null && ./configure >> /dev/null ',
+         shell=True)
     command = 'cd php-src && time make -s -j ' + str(os.getenv(
-        'SERVER_CORES', '1')) + '>> test_result.log '
+        'SERVER_CORES', '1'))
     print(command)
-    print(call('cd php-src && time make -s -j ' + str(os.getenv(
-        'SERVER_CORES', '1')) + '>> test_result.log '
-               ,
-               shell=True))
-    f = open(file_name, "r+")
-    data = f.read()
-    print(data)
-
-
-# return parse_result(data)
-
-
-def parse_result(result=None):
-    parsed_result = {
-        'iterations_per_sec': 0,
-    }
-    data = result.replace('2K performance run parameters for coremark.', '').replace(' ', '').splitlines()
-    for line in data:
-        if len(line) > 0:
-            _data = line.split(':')
-            if _data[0] == 'Iterations/Sec':
-                parsed_result['iterations_per_sec'] = _data[1]
-
-    return parsed_result
+    call(command, shell=True)
+    sec_end = time.time()
+    return sec_end - sec_start
