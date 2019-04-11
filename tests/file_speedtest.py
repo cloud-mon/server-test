8263ad2e05c6a3a32c4488bc31ff76cd844b1d4c
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2019-04-11 15:47:38 +0200

    Add max runtime

--- tests/file_speedtest.py
+++ tests/file_speedtest.py
@@ -23,7 +23,7 @@ def perform_test(volume=False):
         file_name = path + "tmp_result.json"
         filesize = "10"
 
-    command = 'fio --output-format=json --filesize=' + filesize + 'g --ioengine=libaio --filename=' + file + ' --overwrite=1 --invalidate=0 --direct=1 --randrepeat=0 --iodepth=64 --size=4097152k --blocksize=4k --name=random_write --rw=randwrite --end_fsync=1 --name=random_read --stonewall --rw=randread --name=mixed_randrw --stonewall --rw=randrw --rwmixread=90 --rwmixwrite=10 --end_fsync=1 >> ' + file_name,
+    command = 'fio --output-format=json --filesize=' + filesize + 'g --ioengine=libaio --filename=' + file + ' --overwrite=1 --invalidate=0 --direct=1 --randrepeat=0 --iodepth=64 --size=4097152k --blocksize=4k --name=random_write --rw=randwrite --end_fsync=1 --name=random_read --stonewall --rw=randread --name=mixed_randrw --stonewall --rw=randrw --rwmixread=90 --rwmixwrite=10 --end_fsync=1 --runtime=600>> ' + file_name,
     print(command)
     call(command,
          shell=True)
